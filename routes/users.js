205e6f987fc818a703ede4a0330dd2dc76dd7c22
abel-parada <abel.parada.millan@gmail.com>
2022-03-28 19:26:53 +0300

    created:router.param

--- routes/users.js
+++ routes/users.js
@@ -15,10 +15,28 @@ router.post('/', (req,res) => {
     res.send('Create User');
 });
 
-//get by id
-router.get('/:id',(req,res) => {
-    const userId = req.params.id
-    res.send(`Get User with id: ${req.params.id}`);
-});
+//get/put/delete by id
+router.route('/:id')
+    .get((req,res) => {
+        console.log(req.user)
+        const userId = req.params.id;
+        res.send(`Get User with id: ${req.params.id}`);
+    })
+    .put((req,res) => {
+        const userId = req.params.id;
+        res.send(`Update User with id: ${req.params.id}`);
+    })
+    .delete((req,res) => {
+        const userId = req.params.id;
+        res.send(`Delete User with id: ${req.params.id}`);
+    });
+
+const users = [{name:"Jose"},{name:"Abel"}]
+router.param('id', (req,res,next,id) => {
+    // console.log(id);
+    req.user = users[id];
+    next();//this continues on with the rest of our code, starting in the get method (line 20)
+})
+
 
 module.exports = router;
